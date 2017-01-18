74a1f88b299bf463a4c9adaa0aad2671a26c46a7
Paul Verest <paul.verest@live.com>
2017-01-18 22:53:38 +0800

    com.example.jug.gradle-maven

---
+++ src/main/java/com/example/jug/gradlemaven/MainClass.java
@@ -0,0 +1,15 @@
+package com.example.jug.gradlemaven;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainClass {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MainClass.class);
+
+	public static void main(String[] args) {
+		//System.out.println("hello at JUG!");
+		LOG.info("Log we were here");
+	}
+
+}
